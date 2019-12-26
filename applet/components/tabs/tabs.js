f6796a10dbf69a0ef380a27f8b3eaf83f241af69
xuebo.zhan <xuebo.zhan@17zuoye.com>
2019-12-26 20:51:40 +0800

    商品列表

--- applet/components/tabs/tabs.js
+++ applet/components/tabs/tabs.js
@@ -4,7 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    propTabsList:{
+      type:Array,
+      value:[]
+    }
   },
 
   /**
@@ -18,6 +21,17 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
-  }
+    //切换tab
+    tabSelect (e){
+      this.triggerEvent('setTab',{id:e.currentTarget.dataset.id})
+    }
+  },
+  lifetimes: {
+    attached: function() {
+      // 在组件实例进入页面节点树时执行
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+    },
+  },
 })
