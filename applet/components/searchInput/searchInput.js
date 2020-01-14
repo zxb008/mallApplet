ca4b8a9eac2136d805da27b74da20e42a207b339
xuebo.zhan <xuebo.zhan@17zuoye.com>
2020-01-14 15:07:52 +0800

    搜索中心

--- applet/components/searchInput/searchInput.js
+++ applet/components/searchInput/searchInput.js
@@ -18,6 +18,10 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    handleNav(){
+      wx.navigateTo({
+        url: '/pages/search/index'
+      })
+    }
   }
 })
--- applet/pages/search/index.js
+++ applet/pages/search/index.js
@@ -1,13 +1,50 @@
 // pages/search/index.js
+import { request } from '../../request/index'
+import { debounce } from '../../utils/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: []
   },
-
+  timer:-1,
+  debounce: function (func, wait) {
+    return () => {
+      clearTimeout(timer);
+      timer = setTimeout(func, wait);
+    };
+  },
+  clearContain() {
+    this.setData({
+      goods: []
+    })
+  },
+  handNav(e) {
+    wx.navigateTo({
+      url: '/pages/goods_detail/index?gid=' + e.currentTarget.dataset.gid,
+    })
+  },
+  getGoods(query) {
+    request({
+      url: '/goods/qsearch',
+      data: {
+        query
+      }
+    }).then((res) => {
+      this.setData({
+        goods: res
+      })
+    })
+  },
+  handleSearch(e) {
+    const { value } = e.detail
+    if (!value.trim()) {
+      return;
+    }
+    this.debounce(this.getGoods(value).bind(this), 2500)()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
